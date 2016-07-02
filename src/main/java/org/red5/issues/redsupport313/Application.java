09330258db2ac24f9e4da4332f83041361a55e19
Paul Gregoire <mondain@gmail.com>
2016-07-01 21:51:56 -0400

    Updated for debugging

--- src/main/java/org/red5/issues/redsupport313/Application.java
+++ src/main/java/org/red5/issues/redsupport313/Application.java
@@ -1,8 +1,10 @@
 package org.red5.issues.redsupport313;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.red5.logging.Red5LoggerFactory;
 import org.red5.server.adapter.MultiThreadedApplicationAdapter;
 import org.red5.server.api.IAttributeStore;
@@ -15,163 +17,168 @@ import org.red5.server.api.so.ISharedObjectBase;
 import org.red5.server.api.so.ISharedObjectListener;
 
 /**
- * Thanks to Remus Negrota for creating the application source for testing this issue.
- * 
- * @author Remus Negrota (remus@nusofthq.com)
+ * Internal IR5 issue https://infrared5.atlassian.net/browse/REDSUPPORT-313
  */
 public class Application extends MultiThreadedApplicationAdapter {
 
-	@Override
-	public boolean roomStart(IScope currentScope) {
-		log.info("roomStart room.toString()=" + currentScope.toString());
-		return true;
-	}
+    private final String soName = "soName";
 
-	@Override
-	public boolean roomConnect(IConnection conn, Object[] params) {
-		// get the client
-		return true;
-	}
+    @Override
+    public boolean roomStart(IScope currentScope) {
+        log.info("roomStart: {}", currentScope.toString());
+        return true;
+    }
 
-	@Override
-	public boolean roomJoin(IClient client, IScope room) {
-		log.info("roomJoin(" + client.toString() + "," + room.toString() + ")");
+    @Override
+    public boolean roomConnect(IConnection conn, Object[] params) {
+        // get the client
+        return true;
+    }
 
-		IScope currentScope = Red5.getConnectionLocal().getScope();
-		ISharedObject so = getSharedObject(currentScope, "example_so", false);
+    @Override
+    public boolean roomJoin(IClient client, IScope room) {
+        log.info("roomJoin: {} {}", client, room.toString());
+        IScope currentScope = Red5.getConnectionLocal().getScope();
+        ISharedObject so = getSharedObject(currentScope, soName, false);
+        ExampleObject obj = null;
+        if (so.hasAttribute("object1")) {
+            // if the object exists in the so already, update it with the new client
+            obj = (ExampleObject) so.getAttribute("object1");
+            obj.clientId.add(client.getId());
+        } else {
+            // create a new object
+            obj = new ExampleObject();
+            obj.siteId = "Example String";
+            obj.clientId.add(client.getId());
+            // set a listener the first time we create the obj
+            so.addSharedObjectListener(new SOListener());
+            so.setAttribute("object1", obj);
+        }
+        return true;
+    }
 
-		ExampleObject obj = null;
-		if (so.hasAttribute("object1")) {
-			// if the object exists in the so already, update it with the new client
-			obj = (ExampleObject) so.getAttribute("object1");
-			obj.clientId.add(client.getId());
-		} else {
-			// create a new object
-    		obj = new ExampleObject();
-    		obj.siteId = "Example String";
-    		obj.clientId.add(client.getId());
-    		// set a listener the first time we create the obj
-    		so.addSharedObjectListener(new SOListener());
-		}
-		so.setAttribute("object1", obj);
-		
-		return true;
-	}
+    @Override
+    public void roomDisconnect(IConnection conn) {
+        log.info("roomDisconnect(" + conn.toString() + ")");
+    }
 
-	@Override
-	public void roomDisconnect(IConnection conn) {
-		log.info("roomDisconnect(" + conn.toString() + ")");
-	}
+    @Override
+    public void roomLeave(IClient client, IScope room) {
+        log.debug("roomLeave(" + client.toString() + "," + room.toString() + ")");
+    }
 
-	@Override
-	public void roomLeave(IClient client, IScope room) {
-		log.debug("roomLeave(" + client.toString() + "," + room.toString() + ")");
-	}
+    @Override
+    public void roomStop(IScope room) {
+        log.debug("roomStop(" + room.toString() + ")");
+    }
 
-	@Override
-	public void roomStop(IScope room) {
-		log.debug("roomStop(" + room.toString() + ")");
-	}
+    @Override
+    public boolean appStart(IScope app) {
+        log = Red5LoggerFactory.getLogger(Application.class, app.getName());
+        log.info("appStart(" + app.getContextPath() + " " + app.toString() + ") Application Started.");
 
-	@Override
-	public boolean appStart(IScope app) {
-		log = Red5LoggerFactory.getLogger(Application.class, app.getName());
-		log.info("appStart(" + app.getContextPath() + " " + app.toString() + ") Application Started.");
+        return true;
+    }
 
-		return true;
-	}
+    @Override
+    public boolean appConnect(IConnection conn, Object[] params) {
+        log.info("appConnect(" + conn.toString() + "," + params.toString() + ")");
+        return true;
+    }
 
-	@Override
-	public boolean appConnect(IConnection conn, Object[] params) {
-		log.info("appConnect(" + conn.toString() + "," + params.toString() + ")");
-		return true;
-	}
+    @Override
+    public boolean appJoin(IClient client, IScope app) {
+        log.info("appJoin(" + client.toString() + "," + app.toString() + ")");
+        return true;
+    }
 
-	@Override
-	public boolean appJoin(IClient client, IScope app) {
-		log.info("appJoin(" + client.toString() + "," + app.toString() + ")");
-		return true;
-	}
+    @Override
+    public void appDisconnect(IConnection conn) {
+        log.info("appDisconnect(" + conn.toString() + ")");
 
-	@Override
-	public void appDisconnect(IConnection conn) {
-		log.info("appDisconnect(" + conn.toString() + ")");
+    }
 
-	}
+    @Override
+    public void appLeave(IClient client, IScope app) {
+        log.info("appLeave(" + client.toString() + "," + app.toString() + ")");
+        // we make sure the external users list is updated every time a user
+        // leaves the chat
+    }
 
-	@Override
-	public void appLeave(IClient client, IScope app) {
-		log.info("appLeave(" + client.toString() + "," + app.toString() + ")");
-		// we make sure the external users list is updated every time a user
-		// leaves the chat
-	}
+    @Override
+    public void appStop(IScope app) {
+        log.info("appStop(" + app.toString() + ")");
+    }
 
-	@Override
-	public void appStop(IScope app) {
-		log.info("appStop(" + app.toString() + ")");
-	}
+    public void updateSo(int randomNr1, int randomNr2) {
+        log.info("updateSo(" + randomNr1 + " " + randomNr2 + ")");
+        try {
+            IConnection conn = Red5.getConnectionLocal();
+            ISharedObject so = getSharedObject(conn.getScope(), soName, false);
+            //so.beginUpdate(conn);
+            ExampleObject obj = (ExampleObject) so.getAttribute("object1");
+            log.info("updateSo old VALUES(" + obj.random1 + " " + obj.random2 + ")");
+            obj.random1 = randomNr1;
+            obj.random2 = randomNr2;
+            log.info("updateSo new VALUES(" + obj.random1 + " " + obj.random2 + ")");
+            so.setAttribute("object1", obj);
+            // set dirty flag if modifying the attribute object directly
+            so.setDirty(true);
+            //so.endUpdate();
+        } catch (Exception e) {
+            log.warn("Ex on updateSo", e);
+        }
+    }
 
-	public void updateSo(int randomNr1, int randomNr2) {
-		log.info("updateSo(" + randomNr1 + " " + randomNr2 + ")");
+    public void getMap(Map<String, Object> map) {
+        log.info("getMap: {}", map);
+        IConnection conn = Red5.getConnectionLocal();
+        ISharedObject so = getSharedObject(conn.getScope(), soName);
+        if (map == null) {
+            map = new HashMap<>();
+        }
+        map.put("key1", RandomStringUtils.randomAscii(6));
+        map.put("key2", RandomStringUtils.randomAscii(6));
+        // set a listener the first time we create the obj
+        so.addSharedObjectListener(new SOListener());
+        so.setAttribute("attributeMap", map);
+        so.setDirty(true);
+    }
+    
+    private final class SOListener implements ISharedObjectListener {
 
-		IConnection conn = Red5.getConnectionLocal();
-		
-		IScope currentScope = conn.getScope();
-		ISharedObject so = getSharedObject(currentScope, "example_so", false);
+        public void onSharedObjectClear(ISharedObjectBase so) {
+            log.debug("onSharedObjectClear: {}", so);
+        }
 
-//		so.beginUpdate(conn);
-		
-		ExampleObject obj = (ExampleObject) so.getAttribute("object1");
-		log.info("updateSo old VALUES(" + obj.random1 + " " + obj.random2 + ")");
+        public void onSharedObjectConnect(ISharedObjectBase so) {
+            log.debug("onSharedObjectConnect: {}", so);
+        }
 
-		obj.random1 = randomNr1;
-		obj.random2 = randomNr2;
-		
-		// set dirty flag if modifying the attribute object directly
-		so.setDirty(true);
+        public void onSharedObjectDelete(ISharedObjectBase so, String key) {
+            log.debug("onSharedObjectDelete key: {}", key);
+        }
 
-		log.info("updateSo new VALUES(" + obj.random1 + " " + obj.random2 + ")");
+        public void onSharedObjectDisconnect(ISharedObjectBase so) {
+            log.debug("onSharedObjectDisconnect: {}", so);
+        }
 
-		so.setAttribute("object1", obj);
-		
-//		so.endUpdate();		
-		
-	}
-	
-	private final class SOListener implements ISharedObjectListener {
-		
-		public void onSharedObjectClear(ISharedObjectBase so) {		
-			log.debug("onSharedObjectClear: {}", so);
-		}
+        public void onSharedObjectSend(ISharedObjectBase so, String method, List<?> attributes) {
+            log.debug("onSharedObjectSend - method: {} {}", method, attributes);
+        }
 
-		public void onSharedObjectConnect(ISharedObjectBase so) {		
-			log.debug("onSharedObjectConnect: {}", so);
-		}
+        public void onSharedObjectUpdate(ISharedObjectBase so, IAttributeStore attributes) {
+            log.debug("onSharedObjectUpdate - {}", attributes);
+        }
 
-		public void onSharedObjectDelete(ISharedObjectBase so, String key) {		
-			log.debug("onSharedObjectDelete key: {}", key);
-		}
+        public void onSharedObjectUpdate(ISharedObjectBase so, Map<String, Object> attributes) {
+            log.debug("onSharedObjectUpdate - {}", attributes);
+        }
 
-		public void onSharedObjectDisconnect(ISharedObjectBase so) {		
-			log.debug("onSharedObjectDisconnect: {}", so);
-		}
+        public void onSharedObjectUpdate(ISharedObjectBase so, String key, Object value) {
+            log.debug("onSharedObjectUpdate - {} = {}", key, value);
+        }
 
-		public void onSharedObjectSend(ISharedObjectBase so, String method, List<?> attributes) {
-			log.debug("onSharedObjectSend - method: {} {}", method, attributes);
-		}
+    }
 
-		public void onSharedObjectUpdate(ISharedObjectBase so, IAttributeStore attributes) {	
-			log.debug("onSharedObjectUpdate - {}", attributes);	
-		}
-
-		public void onSharedObjectUpdate(ISharedObjectBase so, Map<String, Object> attributes) {
-			log.debug("onSharedObjectUpdate - {}", attributes);
-		}
-
-		public void onSharedObjectUpdate(ISharedObjectBase so, String key, Object value) {
-			log.debug("onSharedObjectUpdate - {} = {}", key, value);
-		}
-		
-	}
-	
 }
--- src/main/java/org/red5/issues/redsupport313/ExampleObject.java
+++ src/main/java/org/red5/issues/redsupport313/ExampleObject.java
@@ -3,64 +3,64 @@ package org.red5.issues.redsupport313;
 import java.util.ArrayList;
 
 public class ExampleObject extends Object {
-	
-	public int random1 = 0;
 
-	public int random2 = 0;
+    public int random1 = 0;
 
-	public long ct = 0;
+    public int random2 = 0;
 
-	public Boolean closed = false;
+    public long ct = 0;
 
-	public String siteId = "";
+    public Boolean closed = false;
 
-	public ArrayList<String> clientId = new ArrayList<String>();
+    public String siteId = "";
 
-	public ExampleObject() {
-		ct = System.currentTimeMillis();
-	}
+    public ArrayList<String> clientId = new ArrayList<String>();
 
-	@Override
+    public ExampleObject() {
+        ct = System.currentTimeMillis();
+    }
+
+    @Override
     public int hashCode() {
-	    final int prime = 31;
-	    int result = 1;
-	    result = prime * result + ((clientId == null) ? 0 : clientId.hashCode());
-	    result = prime * result + (int) (ct ^ (ct >>> 32));
-	    result = prime * result + random1;
-	    result = prime * result + random2;
-	    result = prime * result + ((siteId == null) ? 0 : siteId.hashCode());
-	    return result;
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((clientId == null) ? 0 : clientId.hashCode());
+        result = prime * result + (int) (ct ^ (ct >>> 32));
+        result = prime * result + random1;
+        result = prime * result + random2;
+        result = prime * result + ((siteId == null) ? 0 : siteId.hashCode());
+        return result;
     }
 
-	@Override
+    @Override
     public boolean equals(Object obj) {
-	    if (this == obj)
-		    return true;
-	    if (obj == null)
-		    return false;
-	    if (getClass() != obj.getClass())
-		    return false;
-	    ExampleObject other = (ExampleObject) obj;
-	    if (ct != other.ct)
-		    return false;
-	    if (random1 != other.random1)
-		    return false;
-	    if (random2 != other.random2)
-		    return false;
-	    if (siteId == null) {
-		    if (other.siteId != null)
-			    return false;
-	    } else if (!siteId.equals(other.siteId))
-		    return false;
-	    if (clientId.size() == other.clientId.size()) {
-	    	return false;
-	    }
-	    return true;
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ExampleObject other = (ExampleObject) obj;
+        if (ct != other.ct)
+            return false;
+        if (random1 != other.random1)
+            return false;
+        if (random2 != other.random2)
+            return false;
+        if (siteId == null) {
+            if (other.siteId != null)
+                return false;
+        } else if (!siteId.equals(other.siteId))
+            return false;
+        if (clientId.size() == other.clientId.size()) {
+            return false;
+        }
+        return true;
     }
 
-	@Override
+    @Override
     public String toString() {
-	    return "ExampleObject [random1=" + random1 + ", random2=" + random2 + ", ct=" + ct + ", closed=" + closed + ", siteId=" + siteId + ", clientId=" + clientId + "]";
+        return "ExampleObject [random1=" + random1 + ", random2=" + random2 + ", ct=" + ct + ", closed=" + closed + ", siteId=" + siteId + ", clientId=" + clientId + "]";
     }
-	
+
 }
